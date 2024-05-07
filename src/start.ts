bb0d44ef75be18627fef2b4749ce7a5ecc6ac64b
Drylian <109999325+drylian@users.noreply.github.com>
2024-05-07 19:58:31 -0300

    att

--- src/start.ts
+++ src/start.ts
@@ -1,10 +1,12 @@
 import { GiteeRelease } from "./interfaces/gitee"
 import { writeFileSync } from 'fs'
+import fs from "fs";
 
 async function start() {
     const panels = [
         { name: 'survivalcraft-api', path: 'zaihuishouzh/survivalcraft-api' }
     ]
+    await fs.promises.mkdir("./version", {recursive:true})
     for (const { name, path } of panels) {
         const response = await fetch(`https://gitee.com/api/v5/repos/${path}/releases`)
         const data = await response.json() as GiteeRelease
